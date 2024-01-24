b199b7b6ac8f40a75c124fb98658ca711fe7824d
SuprLazr <ericrc87@gmail.com>
2024-01-24 10:11:08 -0500

    added line 12 for testing

--- Python/Function Processes/ex_swapsort.py
+++ Python/Function Processes/ex_swapsort.py
@@ -1,14 +1,15 @@
 def randomList():
     import random
-    listLength = random.randrange(3,10)
+    listLength = random.randrange(3,15)
     preSort = []
     while len(preSort) < listLength:
-        preSort.append(random.randrange(-999,999))
+        preSort.append(random.randrange(-99999,99999))
     return preSort
 
 def swapSort():
     currentIndex=0
     list = randomList()
+    print(sorted(list))
     while currentIndex<len(list):
         smallest = list[-1]
         smallestIndex=len(list)-1
